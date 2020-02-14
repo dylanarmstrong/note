a105cdb8fa639927537c8bbe1717b2b947fd0105
Dylan Armstrong <dylan@dylan.is>
2020-02-14 16:25:27 -0600

    note cant be empty and dont check date if we dont have to

--- note.py
+++ note.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python3
-
 from sys import argv
 from time import strftime, localtime
 from yaml import load, dump, CLoader as Loader, CDumper as Dumper
 import os
 
 def main():
+    if len(argv) == 1:
+        print('note [note text]')
+        exit(1)
+
     data = ''
     path = '{}/notes.yml'.format(os.environ['HOME'])
     local = localtime()
@@ -16,11 +19,10 @@ def main():
         with open(path) as file:
             data += file.read()
         yml = load(data, Loader=Loader)
+        if date not in yml:
+            yml[date] = []
     else:
-        yml = dict()
-
-    if date not in yml:
-        yml[date] = []
+        yml = dict({ date: [] })
 
     note = ' '.join(argv[1:])
     yml[date].append('({}) {}'.format(time, note))
