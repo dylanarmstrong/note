ac2e42c762f8f06700f75123f603d204423a27c5
Dylan Armstrong <dylan@dylan.is>
2022-02-03 18:13:39 -0600

    chore: isc and black formatter applied

--- note.py
+++ note.py
@@ -3,56 +3,58 @@ from pathlib import Path
 import argparse
 import sqlite3
 
+
 def main():
-    parser = argparse.ArgumentParser(description='Take Notes')
-    parser.add_argument('--read', action='store_true', help='Read current notes')
-    parser.add_argument('words', type=str, nargs='*', help='All the words to put in note', default='')
+    parser = argparse.ArgumentParser(description="Take Notes")
+    parser.add_argument("--read", action="store_true", help="Read current notes")
+    parser.add_argument(
+        "words", type=str, nargs="*", help="All the words to put in note", default=""
+    )
     args = parser.parse_args()
 
     home = str(Path.home())
-    path = f'{home}/notes.db'
+    path = f"{home}/notes.db"
 
     read = args.read
-    words = ' '.join(args.words)
+    words = " ".join(args.words)
     if not read and len(words) == 0:
         print(parser.format_help())
         exit(1)
 
-    con = sqlite3.connect(
-        path,
-        detect_types=sqlite3.PARSE_DECLTYPES
-    )
+    con = sqlite3.connect(path, detect_types=sqlite3.PARSE_DECLTYPES)
     cur = con.cursor()
 
     if read:
-        cur.execute('select date, note from notes order by date desc')
-        prev_date = ''
+        cur.execute("select date, note from notes order by date desc")
+        prev_date = ""
         for [stamp, note] in cur.fetchall():
-            date = stamp.strftime('%B %d, %Y')
+            date = stamp.strftime("%B %d, %Y")
             if prev_date != date:
-                if prev_date != '':
-                    print('')
-                print(f'{date}:')
+                if prev_date != "":
+                    print("")
+                print(f"{date}:")
                 prev_date = date
-            time = stamp.strftime('%H:%M')
-            space = ' ' * len(f'- ({time}) ')
-            note = note.replace('\n', f'\n{space}')
+            time = stamp.strftime("%H:%M")
+            space = " " * len(f"- ({time}) ")
+            note = note.replace("\n", f"\n{space}")
 
-            print(f'- ({time}) {note}')
+            print(f"- ({time}) {note}")
 
     else:
-        cur.execute('''
+        cur.execute(
+            """
             create table if not exists notes (
                 id integer primary key,
                 date timestamp default (datetime('now', 'localtime')),
                 note text
-            )'''
+            )"""
         )
 
-        cur.execute('insert into notes (note) values (?)', [words])
+        cur.execute("insert into notes (note) values (?)", [words])
         con.commit()
 
     con.close()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
